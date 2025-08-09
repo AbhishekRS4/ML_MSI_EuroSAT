3568701bf2938ae1194f5b51698ffb073b4213e4
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2025-08-09 22:16:28 +0530

    [trainer/train_supervised] added more mlflow logging

--- src/trainer/train_supervised.py
+++ src/trainer/train_supervised.py
@@ -366,6 +366,13 @@ def train_pipeline(
         mlflow.log_param("dataset.data_bands", data_bands)
         mlflow.log_param("dataset.num_classes", num_classes)
         mlflow.log_param("dataset.dir_dataset", dir_dataset)
+        mlflow.log_param("dataset.list_class_names", list_class_names)
+        mlflow.log_param("dataset.validation_size", validation_size)
+
+        mlflow.log_param("model.model_name", model_name)
+        mlflow.log_param("model.list_filters", list_filters)
+        mlflow.log_param("model.num_input_bands", num_input_bands)
+        mlflow.log_param("model.dropout_ratio", dropout_ratio)
 
         for epoch in range(1, num_epochs + 1):
             time_start = time.time()
